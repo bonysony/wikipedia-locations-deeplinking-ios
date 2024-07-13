74bf630820374752ffaeea596a46aaba3606caab
Melina Ariyani <ariyani.melina@gmail.com>
2024-07-13 14:23:57 +0200

    Create WikiPlaces test app project files

---
+++ WikiPlaces/WikiPlaces/WikiPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiPlacesApp.swift
+//  WikiPlaces
+//
+//  Created by Melina Ariyani on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WikiPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
