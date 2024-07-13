989a5c3745e147588e54561e39d1c305a30ecb1a
Melina Ariyani <ariyani.melina@gmail.com>
2024-07-13 14:41:29 +0200

    [WikiPlaces] Create Location object model

---
+++ WikiPlaces/WikiPlaces/Models/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  WikiPlaces
+//
+//  Created by Melina Ariyani on 13/07/2024.
+//
+
+import Foundation
+
+struct Location: Codable {
+    let name: String?
+    let lat: Double
+    let lon: Double
+}
