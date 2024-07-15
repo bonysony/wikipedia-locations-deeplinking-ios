c1aa18990a2abcacab2e58954029fcfc87cc3b59
Melina Ariyani <ariyani.melina@gmail.com>
2024-07-15 20:54:24 +0200

    [WikiPlaces] Move previous ContentView code to new view LocationsListVIew

---
+++ WikiPlaces/WikiPlaces/Views/LocationsListView.swift
@@ -0,0 +1,73 @@
+//
+//  LocationsListView.swift
+//  WikiPlaces
+//
+//  Created by Melina Ariyani on 13/07/2024.
+//
+import SwiftUI
+
+struct LocationsListView: View {
+    @ObservedObject var viewModel: LocationViewModel
+    @State private var showAddLocationSheet = false
+
+    var body: some View {
+        
+            List(viewModel.locations) { location in
+                Button(action: {
+                    Task {
+                        await viewModel.openLocation(location)
+                    }
+                }) {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(location.displayName)
+                                .font(.title2)
+                                .bold()
+                            HStack {
+                                Text("Lat:")
+                                    .font(.body)
+                                Text("\(location.lat)")
+                                    .truncationMode(.tail)
+                                    .lineLimit(0)
+                                    .font(.body)
+                            }
+                            HStack{
+                                Text("Long:")
+                                    .font(.body)
+                                Text("\(location.long)")
+                                    .font(.body)
+                                    .truncationMode(.tail)
+                                    .lineLimit(0)
+                            }
+                        }
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }
+                }
+                .contentShape(Rectangle())
+            }
+            .navigationTitle("Locations")
+            .navigationBarItems(trailing: HStack {
+                Button(action: {
+                    showAddLocationSheet = true
+                }) {
+                    Image(systemName: "plus.circle.fill")
+                }
+            })
+            .alert(isPresented: .constant(viewModel.errorMessage != nil)) {
+                Alert(
+                    title: Text("Error"),
+                    message: Text(viewModel.errorMessage ?? "Unknown error"),
+                    dismissButton: .default(Text("OK"))
+                )
+            }
+            .background(Color(UIColor.systemGray6))
+            .sheet(isPresented: $showAddLocationSheet) {
+                AddCustomLocationView(viewModel: viewModel)
+            }
+        }
+}
+
+#Preview {
+    ContentView(viewModel: LocationViewModel(locationService: MockLocationService(), coordinator: MockCoordinator()))
+}
