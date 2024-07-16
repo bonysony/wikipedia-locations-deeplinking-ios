8379d29af51bb591da6ced64eef60faa915e366a
Melina Ariyani <ariyani.melina@gmail.com>
2024-07-16 03:08:12 +0200

    [WikiPlaces] Fix LocationsListVIew file formatting

--- WikiPlaces/WikiPlaces/Views/LocationsListView.swift
+++ WikiPlaces/WikiPlaces/Views/LocationsListView.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Melina Ariyani on 13/07/2024.
 //
+
 import SwiftUI
 
 struct LocationsListView: View {
@@ -11,61 +12,60 @@ struct LocationsListView: View {
     @State private var showAddLocationSheet = false
 
     var body: some View {
-        
-            List(viewModel.locations) { location in
-                Button(action: {
-                    Task {
-                        await viewModel.openLocation(location)
-                    }
-                }) {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(location.displayName)
-                                .font(.title2)
-                                .bold()
-                            HStack {
-                                Text("Lat:")
-                                    .font(.body)
-                                Text("\(location.lat)")
-                                    .truncationMode(.tail)
-                                    .lineLimit(0)
-                                    .font(.body)
-                            }
-                            HStack{
-                                Text("Long:")
-                                    .font(.body)
-                                Text("\(location.long)")
-                                    .font(.body)
-                                    .truncationMode(.tail)
-                                    .lineLimit(0)
-                            }
+        List(viewModel.locations) { location in
+            Button(action: {
+                Task {
+                    await viewModel.openLocation(location)
+                }
+            }) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(location.displayName)
+                            .font(.title2)
+                            .bold()
+                        HStack {
+                            Text("Lat:")
+                                .font(.body)
+                            Text("\(location.lat)")
+                                .truncationMode(.tail)
+                                .lineLimit(0)
+                                .font(.body)
                         }
-                        Spacer()
-                        Image(systemName: "chevron.right")
+                        HStack {
+                            Text("Long:")
+                                .font(.body)
+                            Text("\(location.long)")
+                                .font(.body)
+                                .truncationMode(.tail)
+                                .lineLimit(0)
+                        }
                     }
+                    Spacer()
+                    Image(systemName: "chevron.right")
                 }
-                .contentShape(Rectangle())
             }
-            .navigationTitle("Locations")
-            .navigationBarItems(trailing: HStack {
-                Button(action: {
-                    showAddLocationSheet = true
-                }) {
-                    Image(systemName: "plus.circle.fill")
-                }
-            })
-            .alert(isPresented: .constant(viewModel.error != nil)) {
-                Alert(
-                    title: Text("Error"),
-                    message: Text(viewModel.error?.localizedDescription ?? "Unknown error"),
-                    dismissButton: .default(Text("OK"))
-                )
-            }
-            .background(Color(UIColor.systemGray6))
-            .sheet(isPresented: $showAddLocationSheet) {
-                AddCustomLocationView(viewModel: viewModel)
-            }
+            .contentShape(Rectangle())
         }
+        .navigationTitle("Locations")
+        .navigationBarItems(trailing: HStack {
+            Button(action: {
+                showAddLocationSheet = true
+            }) {
+                Image(systemName: "plus.circle.fill")
+            }
+        })
+        .alert(isPresented: .constant(viewModel.error != nil)) {
+            Alert(
+                title: Text("Error"),
+                message: Text(viewModel.error?.localizedDescription ?? "Unknown error"),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+        .background(Color(UIColor.systemGray6))
+        .sheet(isPresented: $showAddLocationSheet) {
+            AddCustomLocationView(viewModel: viewModel)
+        }
+    }
 }
 
 #Preview {
